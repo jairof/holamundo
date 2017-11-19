16b8e5de545f4c2021fe7016c6f7a674839954ef
Jairo <Jairo@SONYVAIO-PC>
2017-11-19 12:49:28 -0500

    frtt

--- src/hola/HolaMundo.java
+++ src/hola/HolaMundo.java
@@ -2,7 +2,7 @@ package hola;
 
 public class HolaMundo {
 
-	public static void main(String[] args) {
+	public static void doSomething() {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundos");
 	}
